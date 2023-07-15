582db5cde5b3e396d165a6c5b1e4447e33ed9de1
ThijsLacquet <thijs.lacquet@gmail.com>
2023-07-15 21:21:32 +0200

    Remove default secret; set an empty string for testcases on client instead

--- config/cashregister-bridge.php
+++ config/cashregister-bridge.php
@@ -3,6 +3,6 @@
 // config for EtsvThor/LaravelCashRegisterBridge
 return [
     'service_id' => env('CASHREGISTER_SERVICE_ID'),
-    'secret' => env('CASHREGISTER_SECRET', ''),
+    'secret' => env('CASHREGISTER_SECRET'),
     'base_url' => env('CASHREGISTER_BASE_URL', 'https://finances.thor.edu`'),
 ];
