fd9f6a895be643ac5aca9890ec8c1b5535f4563b
Niek Brekelmans <niek.brekelmans@gmail.com>
2023-08-31 16:31:14 +0200

    redirect to cashregister

--- src/Http/Controllers/CashRegisterController.php
+++ src/Http/Controllers/CashRegisterController.php
@@ -6,7 +6,10 @@ use EtsvThor\CashRegisterBridge\Contracts\HasExternalProductItem;
 use EtsvThor\CashRegisterBridge\Exceptions\HasNoExternalProductItem;
 use EtsvThor\CashRegisterBridge\Exceptions\SetAsPaidFailed;
 use EtsvThor\CashRegisterBridge\Http\Controllers\Traits\VerifiesSignature;
+use EtsvThor\CashRegisterBridge\Http\Requests\RedirectToCashRegisterRequest;
 use EtsvThor\CashRegisterBridge\Http\Requests\SetAsPaidRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class CashRegisterController
 {
@@ -14,16 +17,11 @@ class CashRegisterController
 
     public function setAsPaid(SetAsPaidRequest $request)
     {
-        if (! is_null($error = $this->verifySignature($request, config('cashregister-bridge.secret')))) {
+        if (!is_null($error = $this->verifySignature($request, config('cashregister-bridge.secret')))) {
             return $error;
         }
 
-        $type = $request->validated('type');
-
-        /** @var HasExternalProductItem $productItem */
-        $productItem = $type::findOrFail($request->validated('id'));
-
-        throw_unless($productItem instanceof HasExternalProductItem, HasNoExternalProductItem::class);
+        $productItem = $this->getExternalProductItem($request);
 
         $success = $productItem->setAsExternallyPaid();
 
@@ -33,5 +31,43 @@ class CashRegisterController
             'success' => true,
             'message' => 'The item has been set as paid',
         ];
+    }
+
+    public function redirectToCashRegister(RedirectToCashRegisterRequest $request)
+    {
+        $productItem = $this->getExternalProductItem($request);
+
+        $data = $productItem->toExternalProductItem()->only(
+            'product_type',
+            'product_id',
+            'type',
+            'id'
+        );
+
+        $query = http_build_query($data);
+
+        $hash = hash_hmac('sha256', $query, config('cashregister-bridge.secret'));
+
+        $service_id = config('cashregister-bridge.service_id');
+        $url = Str::of(config('cashregister-bridge.base_url'))
+            ->finish('/')
+            ->append("api/services/{$service_id}/service-items/add-to-cart?")
+            ->append($query)
+            ->toString();
+
+        return redirect($url)
+            ->withHeaders(['X-Signature' => $hash]);
+    }
+
+    protected function getExternalProductItem(Request $request): HasExternalProductItem
+    {
+        $type = $request->validated('type');
+
+        /** @var HasExternalProductItem $productItem */
+        $productItem = $type::findOrFail($request->validated('id'));
+
+        throw_unless($productItem instanceof HasExternalProductItem, HasNoExternalProductItem::class);
+
+        return $productItem;
     }
 }
