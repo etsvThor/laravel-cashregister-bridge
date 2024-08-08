903700061ec6c3a12ca627c31ad4f5b601a8d5cc
Niek Brekelmans <10487997+niekbr@users.noreply.github.com>
2024-08-08 10:51:49 +0200

    Revert "feedback"
    This reverts commit 1e484812f9d9cea10dc169a8975b99090c6b600e.

--- src/Contracts/ShouldPushOnSync.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace EtsvThor\CashRegisterBridge\Contracts;
-
-interface ShouldPushOnSync
-{
-    public function wasRecentlyCreated(): bool;
-}
--- src/Traits/PushesExternalProductItem.php
+++ src/Traits/PushesExternalProductItem.php
@@ -4,7 +4,6 @@ namespace EtsvThor\CashRegisterBridge\Traits;
 
 use EtsvThor\CashRegisterBridge\Contracts\HasExternalProduct;
 use EtsvThor\CashRegisterBridge\Contracts\HasExternalProductItem;
-use EtsvThor\CashRegisterBridge\Contracts\ShouldPushOnSync;
 use EtsvThor\CashRegisterBridge\Jobs\DeleteExternalProductItem;
 use EtsvThor\CashRegisterBridge\Jobs\PushExternalProduct;
 use EtsvThor\CashRegisterBridge\Jobs\PushExternalProductItem;
@@ -15,7 +14,7 @@ trait PushesExternalProductItem
     public static function bootPushesExternalProductItem(): void
     {
         static::saved(function (HasExternalProductItem $externalProductItem) {
-            if ($externalProductItem instanceof ShouldPushOnSync && $externalProductItem->wasRecentlyCreated()) {
+            if (config('cashregister-bridge.product_item_saved_sync', false) && $externalProductItem->wasRecentlyCreated) {
                 PushExternalProductItem::dispatch($externalProductItem)->onConnection('sync');;
             } else {
                 PushExternalProductItem::dispatch($externalProductItem);
