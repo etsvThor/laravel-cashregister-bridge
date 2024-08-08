0276c6919f5d63c317eaa6a6fb2403c7a58969d8
Niek Brekelmans <10487997+niekbr@users.noreply.github.com>
2024-08-08 10:56:29 +0200

    oops, I gave wrong feedback

--- config/cashregister-bridge.php
+++ config/cashregister-bridge.php
@@ -2,8 +2,9 @@
 
 // config for EtsvThor/LaravelCashRegisterBridge
 return [
-    'product_item_saved_sync' => false,
     'service_id' => env('CASHREGISTER_SERVICE_ID'),
     'secret' => env('CASHREGISTER_SECRET'),
     'base_url' => env('CASHREGISTER_BASE_URL', 'https://finances.thor.edu'),
+
+    'push_product_items_on_sync' => false,
 ];
--- src/Traits/PushesExternalProductItem.php
+++ src/Traits/PushesExternalProductItem.php
@@ -7,15 +7,16 @@ use EtsvThor\CashRegisterBridge\Contracts\HasExternalProductItem;
 use EtsvThor\CashRegisterBridge\Jobs\DeleteExternalProductItem;
 use EtsvThor\CashRegisterBridge\Jobs\PushExternalProduct;
 use EtsvThor\CashRegisterBridge\Jobs\PushExternalProductItem;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Http;
 
 trait PushesExternalProductItem
 {
     public static function bootPushesExternalProductItem(): void
     {
-        static::saved(function (HasExternalProductItem $externalProductItem) {
-            if (config('cashregister-bridge.product_item_saved_sync', false) && $externalProductItem->wasRecentlyCreated) {
-                PushExternalProductItem::dispatch($externalProductItem)->onConnection('sync');;
+        static::saved(function (HasExternalProductItem&Model $externalProductItem) {
+            if (config('cashregister-bridge.push_product_items_on_sync') && $externalProductItem->wasRecentlyCreated) {
+                PushExternalProductItem::dispatch($externalProductItem)->onConnection('sync');
             } else {
                 PushExternalProductItem::dispatch($externalProductItem);
             }
