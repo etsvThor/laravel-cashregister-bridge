eca9af1fdc76e2d6a03e80544cb3c5975497f6ec
ThijsLacquet <thijs.lacquet@gmail.com>
2024-10-27 12:16:51 +0100

    Fix getExternalProductItem for softdeletes models

--- src/Http/Controllers/CashRegisterController.php
+++ src/Http/Controllers/CashRegisterController.php
@@ -92,7 +92,9 @@ class CashRegisterController
             : ['*'];
 
         /** @var HasExternalProductItem&Model $productItem */
-        if (method_exists($type, 'withThrashed')) {
+
+
+        if (method_exists($type, 'bootSoftDeletes')) {
             $productItem = $type::withTrashed()->findOrFail($id, $columns);
         } else {
             $productItem = $type::findOrFail($id, $columns);
